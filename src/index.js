1cc1bdf1e8ab6f21d369f7d7ab1ba8dcb90be826
jisoorose <ahd.iu2002@gmail.com>
2024-07-31 16:17:32 +0330

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <CardHeader srcAdress="/Boo.png" name="Boo" />
+      <CardBody
+        title="Boo is a developer"
+        description="he learned many langueges like python , c# , c++, django, js, and react that this card was created by react. that's why this card is very cool ."
+      />
+      <section className="footer-wrapper">
+        <CardFooter BgColor="#ff2c2c" achivment="python" emoji="🤩" />
+        <CardFooter BgColor="#5849f1" achivment="django" emoji="🙌" />
+        <CardFooter BgColor="#8c14da" achivment="js" emoji="🏆" />
+        <CardFooter BgColor="#ff617c" achivment="c++" emoji="🎉" />
+      </section>
+    </div>
+  );
+}
+
+function CardHeader(props) {
+  return <img className="img" src={props.srcAdress} alt={props.name} />;
+}
+function CardBody(props) {
+  return (
+    <div className="content-card">
+      <h4>{props.title}</h4>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+function CardFooter(props) {
+  return (
+    <section>
+      <p
+        className="achivment"
+        style={{ backgroundColor: props.BgColor, color: "#fbfbfb" }}
+      >
+        {props.achivment} <span>{props.emoji}</span>
+      </p>
+    </section>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
